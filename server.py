79ad3be58a10c6dad9c734a43206227c709b7c2e
girivvv <130071023+girivvv@users.noreply.github.com>
2024-12-19 11:38:49 +0530

    Update server.py

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ switch_state = {
 }
 
 # Specify the path to the HTML file in the same directory as this script
-HTML_FILE_PATH = "C:\Users\girid\xample\index.py"
+HTML_FILE_PATH = "C:\Users\girid\xample\index.html"
 
 class GetStateHandler(RequestHandler):
     """Handler for returning the current state of switches."""
